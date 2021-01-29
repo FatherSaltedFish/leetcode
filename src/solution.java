e30d315788aa7a51ed99abc492036ccc4ccdcce1
Father salted fish <50800480+1541987221@users.noreply.github.com>
2021-01-29 13:47:08 +0800

    FSF

--- src/solution.java
+++ src/solution.java
@@ -7,14 +7,14 @@ public class solution {
 
     }
 
-//    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
 //1631. 最小体力消耗路径
 //    你准备参加一场远足活动。给你一个二维rows x columns的地图heights，其中heights[row][col]表示格子(row, col)的高度。一开始你在最左上角的格子(0, 0)，且你希望去最右下角的格子(rows-1, columns-1)（注意下标从 0 开始编号）。你每次可以往 上，下，左，右四个方向之一移动，你想要找到耗费 体力 最小的一条路径。
 //
 //    一条路径耗费的 体力值是路径上相邻格子之间 高度差绝对值的 最大值决定的。
 //
 //    请你返回从左上角走到右下角的最小体力消耗值。
-
+//    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     public int minimumEffortPath(int[][] heights) {//迪杰斯特拉算法
         int m = heights.length;
         int n = heights[0].length;

a740c92ba98b6ec41fba887be6ae8981b8c6b3a5
Father salted fish <50800480+1541987221@users.noreply.github.com>
2021-01-29 13:46:38 +0800

    FSF

--- src/solution.java
+++ src/solution.java
@@ -7,6 +7,54 @@ public class solution {
 
     }
 
+//    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+//1631. 最小体力消耗路径
+//    你准备参加一场远足活动。给你一个二维rows x columns的地图heights，其中heights[row][col]表示格子(row, col)的高度。一开始你在最左上角的格子(0, 0)，且你希望去最右下角的格子(rows-1, columns-1)（注意下标从 0 开始编号）。你每次可以往 上，下，左，右四个方向之一移动，你想要找到耗费 体力 最小的一条路径。
+//
+//    一条路径耗费的 体力值是路径上相邻格子之间 高度差绝对值的 最大值决定的。
+//
+//    请你返回从左上角走到右下角的最小体力消耗值。
+
+    public int minimumEffortPath(int[][] heights) {//迪杰斯特拉算法
+        int m = heights.length;
+        int n = heights[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            public int compare(int[] edge1, int[] edge2) {
+                return edge1[2] - edge2[2];
+            }
+        });
+        pq.offer(new int[]{0, 0, 0});
+
+        int[] dist = new int[m * n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
+        boolean[] seen = new boolean[m * n];
+
+        while (!pq.isEmpty()) {
+            int[] edge = pq.poll();
+            int x = edge[0], y = edge[1], d = edge[2];
+            int id = x * n + y;
+            if (seen[id]) {
+                continue;
+            }
+            if (x == m - 1 && y == n - 1) {
+                break;
+            }
+            seen[id] = true;
+            for (int i = 0; i < 4; ++i) {
+                int nx = x + dirs[i][0];
+                int ny = y + dirs[i][1];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && Math.max(d, Math.abs(heights[x][y] - heights[nx][ny])) < dist[nx * n + ny]) {
+                    dist[nx * n + ny] = Math.max(d, Math.abs(heights[x][y] - heights[nx][ny]));
+                    pq.offer(new int[]{nx, ny, dist[nx * n + ny]});
+                }
+            }
+        }
+
+        return dist[m * n - 1];
+    }
+
+
     public int pivotIndex(int[] nums) {
         if(nums.length-1==0)
             return 0;
